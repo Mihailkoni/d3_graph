ec5046a1700eb677c0fc4f165cb277d887182f4d
Mihail <korikov.me@students.dvfu.ru>
2025-04-26 19:52:19 +1000

    Добавил отображение таблицы и фильтров графика. Реализовал показ и скрытие таблицы. Реализовал график и фильтры для него. Добавил стилей.

---
+++ JavaScript/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    showTable('build', buildings);
+    d3.select("input[value='Построить'").on("click", function(){
+        drawGraph(buildings);
+     });
+ })
